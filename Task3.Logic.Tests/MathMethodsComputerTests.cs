9ec5c8771fc071d5d34426f3f3d66740326915e0
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-27 00:46:36 +0300

    Test cases added. Let's try TDD

--- Task3.Logic.Tests/MathMethodsComputerTests.cs
+++ Task3.Logic.Tests/MathMethodsComputerTests.cs
@@ -10,11 +10,26 @@ namespace Task3.Logic.Tests
     [TestFixture]
     public class MathMethodsComputerTests
     {
+        [TestCase(9, 2, 1e-10, 3.0000000000,
+            Description = "Test sqrt.")]
+        [TestCase(15, 3, 1e-6, 2.466212,
+            Description = "Power = 3")]
+        [TestCase(1, 15, 1e-6, 1.000000,
+            Description = "Test sqrtn from one")]
+        [TestCase(0, 10, 1e-8, 0,
+            Description = "Test sqrtn from zero")]
+        [TestCase(5, 1, 1e-6, 5,
+            Description = "Test power = 1")]
+        [TestCase(25489, 9, 1e-6, 3.087413,
+            Description = "Test big power root from big value")]
         [Test]
         public void Sqrtn_ValueEpsilon_RootOfDegreeNReturn
-            (double value, int n, double epsilon, double expected)
+            (double value, int power, double epsilon, double expected)
         {
-            
+            //act
+            double actual = value.Sqrtn(power, epsilon);
+            //assert
+            Assert.LessOrEqual(Math.Abs(actual - expected), epsilon);
         }
     }
 }
