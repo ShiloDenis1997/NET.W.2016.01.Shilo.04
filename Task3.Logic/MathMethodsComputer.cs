ca390ade0a44777f3d3870ae58ec0185b558c584
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-27 00:29:12 +0300

    Task3.Logic created. Sqrtn stub added

---
+++ Task3.Logic/MathMethodsComputer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Logic
+{
+    public static class MathMethodsComputer
+    {
+        public static double Sqrtn(this double value, int power)
+        {
+            return 0;
+        }
+    }
+}
