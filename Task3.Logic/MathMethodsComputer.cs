9fffeefe9f015d9b54db3c6cd5d161e8eb506fc6
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-27 01:32:21 +0300

    Sqrtn almost implemented. Have a problem with small numbers

--- Task3.Logic/MathMethodsComputer.cs
+++ Task3.Logic/MathMethodsComputer.cs
@@ -10,7 +10,26 @@ namespace Task3.Logic
     {
         public static double Sqrtn(this double value, int power, double epsilon)
         {
-            return 0;
+            if (power < 1)
+                throw new ArgumentException("power must be equal or greater than 1");
+            if (value < 0)
+                throw new ArgumentException("vaue must be equal or greater than 0 ");
+            if (epsilon <= 0)
+                throw new ArgumentException("epsilon must be greater than 0");
+            double xk;
+            double xkp1 = 1;
+            do
+            {
+                xk = xkp1;
+                xkp1 = SqrtnNewtonIteration(xk, value, power);
+            } while (Math.Abs(xk - xkp1) > epsilon);
+
+            return xkp1;
+        }
+
+        private static double SqrtnNewtonIteration(double xk, double value, int power)
+        {
+            return ((power - 1) * xk + value / Math.Pow(xk, power - 1)) / power;
         }
     }
 }
