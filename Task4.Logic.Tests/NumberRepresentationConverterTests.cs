aa07421856cd8226e19ce778f1b5c4e4e65d5dc5
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-27 14:11:20 +0300

    Unit tests for DoubleToBinaryString method added. all is green

--- Task4.Logic.Tests/NumberRepresentationConverterTests.cs
+++ Task4.Logic.Tests/NumberRepresentationConverterTests.cs
@@ -13,12 +13,40 @@ namespace Task4.Logic.Tests
     {
         [TestCase(155,
             ExpectedResult = "0100000001100011011000000000000000000000000000000000000000000000",
-            Description = "Positive round number")]
+            Description = "Positive round value")]
         [TestCase(122.625,
             ExpectedResult = "0100000001011110101010000000000000000000000000000000000000000000",
-            Description = "Value with good fraction (power of 5)")]
+            Description = "Positive value with good fraction (power of 5)")]
+        [TestCase(451387.2345,
+            ExpectedResult = "0100000100011011100011001110110011110000001000001100010010011100",
+            Description = "Posititve value with vad fraction (not power of 5)")]
+        [TestCase(-155,
+            ExpectedResult = "1100000001100011011000000000000000000000000000000000000000000000",
+            Description = "Negative round value")]
+        [TestCase(-122.625,
+            ExpectedResult = "1100000001011110101010000000000000000000000000000000000000000000",
+            Description = "Negative value with good fraction (power of 5)")]
+        [TestCase(-451387.2345,
+            ExpectedResult = "1100000100011011100011001110110011110000001000001100010010011100",
+            Description = "Negative value with vad fraction (not power of 5)")]
+        [TestCase(double.NaN,
+            ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000",
+            Description = "double NaN test. It's only a constant value."
+                + " By fact NaN is every value > +inf or < -inf")]
+        [TestCase(double.NegativeInfinity,
+            ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000",
+            Description = "double NegativeInfinity test")]
+        [TestCase(double.PositiveInfinity,
+            ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000",
+            Description = "double PositiveInfinity test")]
+        [TestCase(0.0,
+            ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000000",
+            Description = "+0 test")]
+        [TestCase(-0.0, 
+            ExpectedResult = "1000000000000000000000000000000000000000000000000000000000000000",
+            Description = "-0 test")]
         [Test]
-        public string DoubleToBinaryString(double value)
+        public string DoubleToBinaryString_Double_StringReturn(double value)
         {
             return value.DoubleToBinaryString();
         }
