d6f1cc854c3b626f26058de521b17ac1089a8e3b
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-27 12:42:03 +0300

    Task4.Logic stub creatd

---
+++ Task4.Logic/NumberRepresentationConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Logic
+{
+    public static class NumberRepresentationConverter
+    {
+        //public static const string NaN = ""
+        public static string DoubleToBinaryString(this double x)
+        {
+            if (double.IsNaN(x))
+                return "NaN";
+            StringBuilder sb = new StringBuilder(64);
+
+            return sb.ToString();
+        }
+    }
+}
