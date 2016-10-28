05a08430aa32a6c4f6e72d19109fe49dd23c5166
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-28 22:52:37 +0300

    Task4 and tests for it updated. All is green

--- Task4.Logic/NumberRepresentationConverter.cs
+++ Task4.Logic/NumberRepresentationConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Task4.Logic
@@ -6,7 +7,8 @@ namespace Task4.Logic
     public static class NumberRepresentationConverter
     {
         /// <summary>
-        /// Makes string representation of bits of doubel value x according
+        /// Makes string representation of bits of double value 
+        /// <paramref name="x"/> according
         /// to IEEE754 standart
         /// </summary>
         /// <param name="x"></param>
@@ -16,13 +18,14 @@ namespace Task4.Logic
             var convertStruct = 
                 new DoubleToLongStruct {DoubleBitsRepresentation = x};
             long value = convertStruct.LongBitsRepresentation;
-            var sb = new StringBuilder(64);
-            sb.Append(value < 0 ? '1' : '0');
-            for (int i = 62; i >= 0; i--)
+            int bitsCount = sizeof(double) * 8;
+            char[] result = new char[bitsCount];
+            result[0] = value < 0 ? '1' : '0';
+            for (int i = bitsCount - 2, j = 1; i >= 0; i--, j++)
             {
-                sb.Append((value & ((long)1 << i)) != 0 ? '1' : '0');
+                result[j] = (value & ((long)1 << i)) != 0 ? '1' : '0';
             }
-            return sb.ToString();
+            return new string(result);
         }
 
         /// <summary>
